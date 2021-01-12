ed33e3a342e4edb7433af79d0702bc6472f36f32
DannyRobbs <anerobi86@gmail.com>
2021-01-12 08:00:40 +0100

    last commit

--- app/src/main/java/com/example/trial1/MainActivity.java
+++ app/src/main/java/com/example/trial1/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     //Hello daniel
     //Hello everyone.
     //hello there!
+    //hello for the last time
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
